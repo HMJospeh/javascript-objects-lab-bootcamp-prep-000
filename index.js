33e17f1d89ef16d487aab2ab4650b894f85ec9b4
Humza Joseph <humza1born@gmail.com>
2018-01-04 07:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   delete object.key;
-  return newObj;
+  return object;
 }
