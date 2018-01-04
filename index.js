4084a7faacec77f767a4791a62721ae1cb035e6c
Humza Joseph <humza1born@gmail.com>
2018-01-04 06:38:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes= {
+
+}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+ var newObj = Object.assign({},object,{[key]: value});
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = '2';
+  return object;
+  }
+
+function deleteFromObjectByKey(object, key) {
+
+}
